c7987132417d15bf63d3b5a8158bd80eda3fb499
Miron Balcerzak <mironbalcerzak@gmail.com>
2024-04-27 21:27:01 +0200

    orval/core - factory method generated

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1017,6 +1017,7 @@ export const SchemaType = {
 
 export type ScalarValue = {
   value: string;
+  factoryMethodValue: string;
   isEnum: boolean;
   hasReadonlyProps: boolean;
   type: SchemaType;
